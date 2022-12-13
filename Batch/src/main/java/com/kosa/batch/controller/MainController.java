0a74b02386f40dbdcf822b6e96e8475ace7f0d1d
user <user@DESKTOP-FLE0FB6>
2022-12-13 11:24:13 +0900

    .

--- Batch/src/main/java/com/kosa/batch/controller/MainController.java
+++ Batch/src/main/java/com/kosa/batch/controller/MainController.java
@@ -2,7 +2,7 @@ package com.kosa.batch.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+//dddd
 @Controller
 public class MainController {
 	@GetMapping("/")
