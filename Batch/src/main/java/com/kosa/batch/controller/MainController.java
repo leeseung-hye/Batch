fa5e72f3642906bfd86bfa1f29e310f8a55839d6
정영훈 <yunghun0815@gmail.com>
2022-12-13 11:31:35 +0900

    hello2

--- Batch/src/main/java/com/kosa/batch/controller/MainController.java
+++ Batch/src/main/java/com/kosa/batch/controller/MainController.java
@@ -3,7 +3,7 @@ package com.kosa.batch.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 //dddd
-//hello
+//hello2
 @Controller
 public class MainController {
 	@GetMapping("/")
